e462920b88a15dcfbd19120008250f1606391a09
hidanbi <hidanbi@nate.com>
2025-04-07 00:43:08 +0900

    main func arg name change

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hello(name):
+def print_hello(name_):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'hello, {name_}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
